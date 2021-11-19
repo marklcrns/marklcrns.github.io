04af8c7859b72707c6662bee692c78b368311868
Mark Lucernas <marklcrns@gmail.com>
2021-11-19 10:21:11 -0800

    refactor(blog): Update to bottleneck feeds

--- app/js/blog.js
+++ app/js/blog.js
@@ -1,14 +1,15 @@
 // Generate blog feed
 const FEED_URL = `https://marklucernas.dev/index.xml`;
+const FEED_COUNT = 5;
 
 var feed = ""
 $.get(FEED_URL, function (data) {
   feed += '<h2>Recent Blog Posts</h2>'
         + '<h5><a href="https://marklucernas.dev/">marklucernas.dev</a></h5>'
         + '<div class="posts">';
-  $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+  $(data).find("entry").each(function (i) { // or "item" or whatever suits your feed
       var el = $(this);
-      
+
       var content = el.find("content").text();
       var contentRaw = content.replace(/<\/?[^>]+(>|$)#?/g, " ");
       var ts = new Date(el.find("updated").text());
@@ -25,7 +26,6 @@ $.get(FEED_URL, function (data) {
               + '<div class="flex project-details">'
 
       if (temp.getElementsByTagName("img").length > 0) {
-        console.log(temp.getElementsByTagName("img")[0].outerHTML);
         feed += temp.getElementsByTagName("img")[0].outerHTML
       }
 
@@ -34,6 +34,8 @@ $.get(FEED_URL, function (data) {
             + '</div>'
           + '</div>'
           + '</a>';
+
+      return i <= FEED_COUNT;
   });
   feed += '</div>';
   document.getElementById("blog").
